26b578bf151dc7121d6e4d2b7b3b75c6a5c53deb
sergio sanchez lopez <zergiosanchezlopez@gmail.com>
2023-05-02 17:22:09 +0200

    subida de practica

---
+++ codigo/StockSequence.java
@@ -0,0 +1,16 @@
+package es.uned.lsi.eped.pract2022_2023;
+
+import ...
+
+public class StockSequence implements StockIF {
+
+	protected SequenceIF<StockPair> stock;
+	
+	/* Constructor de la clase */
+	public StockSequence() {
+		...
+	}
+
+	...
+
+}
---
+++ codigo/StockTree.java
@@ -0,0 +1,16 @@
+package es.uned.lsi.eped.pract2022_2023;
+
+import ...
+
+public class StockTree implements StockIF {
+
+	protected GTreeIF<Node> stock; /* El stock es un árbol general de nodos */
+	
+	/* Constructor de la clase */
+	public StockTree() {
+		...
+	}
+
+	...
+	
+}
